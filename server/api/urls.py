40b6b6e473e33be7c22584f69a18d8bee125f23f
bloopgoop <zhu.kevin12@gmail.com>
2024-04-21 22:55:57 -0400

    feat: add jwt for authentication

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import CustomTokenObtainPairView
+
 
 urlpatterns = [
     path("health/", views.health, name="health"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -4,9 +4,26 @@ from django.urls import get_resolver
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['badge_id'] = user.username
+        # ...
+
+        return token
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
 
 @api_view(['GET'])
 def health(request):
     return Response("Server is up and runnning!")
-
-
