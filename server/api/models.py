7aa4de96a049232865f26750cca0f41cbce9c193
JoanG5 <Joanguzman553@gmail.com>
2024-04-24 17:22:00 -0400

    Created a table for the patientData page, created a search but only works with ID currently, will replace with name, DOB, ect.

--- server/api/models.py
+++ server/api/models.py
@@ -8,7 +8,6 @@ from django.core.validators import MinValueValidator
 class UserDiabetesScreeningData(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Associated User")
-    name = models.CharField(max_length=255, help_text="Name of the individual.")
     pregnancies = models.IntegerField(
         validators=[MinValueValidator(0)], 
         help_text="Number of times pregnant."
